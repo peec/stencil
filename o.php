4dcbd50986fded40bb18dbc22d5fa24d171ed7f6
peec <kjelkenes@gmail.com>
2014-12-20 17:50:57 +0100

    Commit tests

---
+++ o.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<title><?php echo 'hello world' ?></title>
+</head>
+<body>
+<h1>Why</h1>
+
+@{$test = %}
+
+@{$test}
+
+<?php
+$i = 0;
+for ($i = 0; $i < 10; $i++) {
+    echo "test";
+}
+?>
+</body>
+</html>
